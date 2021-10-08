079a931f52592e8ae6db8d1d1efc8a6f0f6d512d
Shiv10 <shivanshsharma2012@gmail.com>
2021-10-09 00:34:41 +0530

    feat: create server

--- main.go
+++ main.go
@@ -4,12 +4,16 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	// "go.mongodb.org/mongo-driver/mongo/readpref"
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"context"
 	"time"
+	"net"
+	"flag"
 	"fmt"
+	"net/http"
+	
 )
 
 
@@ -27,6 +31,24 @@ type Posts struct {
 	password string `bson:"passowrd,omitempty"`
 }
 
+func runHttp(listenAddr string) error {
+  s := http.Server{
+    Addr:    listenAddr,
+    Handler: NewRouter(),
+  }
+  fmt.Printf("Starting HTTP listener at %s\n", listenAddr)
+  return s.ListenAndServe()
+}
+
+func NewRouter() http.Handler {
+	main := http.NewServeMux()
+	main.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	  w.WriteHeader(http.StatusOK)
+	  w.Write([]byte("ok"))
+	})
+	return main
+  }
+
 func main(){
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://appointy:1234567890@cluster0.1yuhs.mongodb.net/AppointyDB?retryWrites=true&w=majority"))
 	if err!= nil {
@@ -40,15 +62,22 @@ func main(){
 	defer cancel()
 	defer client.Disconnect(ctx)
 
-	appointyDB := client.Database("AppointyDB")
+	// appointyDB := client.Database("AppointyDB")
 	// usersCollection := appointyDB.Collection("Users")
 	// postsCollection := appointyDB.Collection("Posts")
 
-
-	if err!= nil {
-		log.Fatal("err occurred")
-	}
-
 	fmt.Println("test")
 
+	//sever setup
+
+	var (
+		host = flag.String("host", "", "host http address to listen on")
+		port = flag.String("port", "8000", "port number for http listener")
+	)
+
+	flag.Parse()
+	addr := net.JoinHostPort(*host, *port)
+	if err := runHttp(addr); err != nil {
+		log.Fatal(err)
+	}
 }
