5c34d34bdb9b4084dd0b36f4d493285299e14031
Shiv10 <shivanshsharma2012@gmail.com>
2021-10-08 23:49:54 +0530

    feat: add models

--- main.go
+++ main.go
@@ -4,32 +4,51 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	// "go.mongodb.org/mongo-driver/mongo/readpref"
-	// "go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"context"
 	"time"
-	// "fmt"
-	"os"
+	"fmt"
 )
+
+
+type Users struct {
+	ID primitive.ObjectID `bson:"_id"`
+	Name string `bson:"name,omitempty"`
+	Email string `bson:"email,omitempty"`
+	password string `bson:"passowrd,omitempty"`
+}
+
+type Posts struct {
+	ID primitive.ObjectID `bson:"_id"`
+	Name string `bson:"name,omitempty"`
+	Email string `bson:"email,omitempty"`
+	password string `bson:"passowrd,omitempty"`
+}
 
 func main(){
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://appointy:1234567890@cluster0.1yuhs.mongodb.net/AppointyDB?retryWrites=true&w=majority"))
 	if err!= nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	err = client.Connect(ctx)
 	if err!= nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
 	defer cancel()
 	defer client.Disconnect(ctx)
 
 	appointyDB := client.Database("AppointyDB")
-	usersCollection := appointyDB.Collection("Users")
-	postsCollection := appointyDB.Collection("Posts")
+	// usersCollection := appointyDB.Collection("Users")
+	// postsCollection := appointyDB.Collection("Posts")
 
+
+	if err!= nil {
+		log.Fatal("err occurred")
+	}
+
+	fmt.Println("test")
 
 }
