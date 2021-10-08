30aa9d61e6b19f32c2a6d073fc725cd09ebdc233
Shiv10 <shivanshsharma2012@gmail.com>
2021-10-08 22:55:11 +0530

    feat: add db connection

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	// "go.mongodb.org/mongo-driver/mongo/readpref"
+	// "go.mongodb.org/mongo-driver/bson"
+	"log"
+	"context"
+	"time"
+	// "fmt"
+	"os"
+)
+
+func main(){
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://appointy:1234567890@cluster0.1yuhs.mongodb.net/AppointyDB?retryWrites=true&w=majority"))
+	if err!= nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+
+	if err!= nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer cancel()
+	defer client.Disconnect(ctx)
+
+}
