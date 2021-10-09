395c63a8887fb11affff0196c6e898efa48980e6
Shiv10 <shivanshsharma2012@gmail.com>
2021-10-09 14:56:10 +0530

    fix: code refactoring

--- main.go
+++ main.go
@@ -4,33 +4,17 @@ import (
 	"Appointy-instagram-clone/routers"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	// "go.mongodb.org/mongo-driver/mongo/readpref"
-	// "go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"log"
 	"context"
 	"time"
 	"net"
 	"flag"
-	"fmt"
 )
 
 
-type Users struct {
-	ID primitive.ObjectID `bson:"_id"`
-	Name string `bson:"name,omitempty"`
-	Email string `bson:"email,omitempty"`
-	password string `bson:"passowrd,omitempty"`
-}
-
-type Posts struct {
-	ID primitive.ObjectID `bson:"_id"`
-	Name string `bson:"name,omitempty"`
-	Email string `bson:"email,omitempty"`
-	password string `bson:"passowrd,omitempty"`
-}
-
 func main(){
+
+	//Setup Mongo connection
 	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://appointy:1234567890@cluster0.1yuhs.mongodb.net/AppointyDB?retryWrites=true&w=majority"))
 	if err!= nil {
 		log.Fatal(err)
@@ -43,14 +27,7 @@ func main(){
 	defer cancel()
 	defer client.Disconnect(ctx)
 
-	// appointyDB := client.Database("AppointyDB")
-	// usersCollection := appointyDB.Collection("Users")
-	// postsCollection := appointyDB.Collection("Posts")
-
-	fmt.Println("test")
-
-	//sever setup
-
+	//HTTP Sever setup
 	var (
 		host = flag.String("host", "", "host http address to listen on")
 		port = flag.String("port", "3000", "port number for http listener")
